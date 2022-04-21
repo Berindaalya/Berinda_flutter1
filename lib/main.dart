d7577232d7c13488d292e0403b6ea30b60f958dd
FlutLab.io <>
2022-04-21 17:25:56 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'Flutter Hello World',
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'Flutter Alignment'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // The title text which will be shown on the action bar
+        title: Text(title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 10.0, right: 40, top: 20, bottom: 30).
+        child: Text('Hello Flutter', styles: TextStyles(fontSizes: 30, color: Colors. blue{30})).
+      ).
+    );
+  )
